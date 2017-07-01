05cb7e9e848b697b3f6c45088d82c7530142c4b4
Nguyễn Văn Tân <tannguyenit95@gmail.com>
2017-07-01 13:22:04 +0700

    Add files via upload

---
+++ database/migrations/2017_07_01_061655_edit_threads_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class EditThreadsTable extends Migration
+{
+
+    public function up()
+    {
+        Schema::table('threads', function (Blueprint $table) {
+            $table->string('subject')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('threads', function (Blueprint $table) {
+            $table->string('subject')->nullable();
+        });
+    }
+}
